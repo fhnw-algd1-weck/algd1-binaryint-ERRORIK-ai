52205966e26c8dbe559c30da1ebe219d685b427b
Erik <erik.steinacher@gmail.com>
2025-02-24 11:06:13 +0100

    solution fix

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -2,14 +2,42 @@ package ch.fhnw.algd1.converters.binary;
 
 public class BinConverter {
 	public static String toString(int x) {
-		// TODO expect x to be in range [-128, 127], return string with 8 binary
-		// digits representing x in 2-complement
-		return "00000000";
+		String bitValue = "";
+
+		while (x != 0) {
+			if (x % 2 == 0) { // If is divisible by 2 then add 0 else add 1
+				bitValue = "0" + bitValue;
+			} else {
+				bitValue = "1" + bitValue;
+			}
+			x = x / 2;
+		}
+
+		//fill in leading zeros
+		while (bitValue.length() < 8) {
+			bitValue = "0" + bitValue;
+		}
+
+		return bitValue;
 	}
 
 	public static int fromString(String text) {
-		// TODO expect text to contain 8 binary digits, parse to int value in
-		// 2-complement
-		return 0;
+		int intValue = 0;
+		int text_length = text.length();
+		int text_pos;
+
+		// cleaner version
+		// for (int i = 0; i < text.length(); i++) {
+		// 	intValue = intValue * 2 + (text.charAt(i) - '0'); clean solution
+		// }
+
+		for (int i = 0; i < text.length(); i++) {
+			text_pos = text_length - i - 1;
+			if (text.charAt(i) == '1') {
+				intValue |= (1 << text_pos);
+			}
+		}
+	
+		return intValue;
 	}
 }
